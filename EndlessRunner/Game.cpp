9d9ec28456d52e0bed05de4fc063abb467ae9603
Ivan <iv_fomenko@bk.ru>
2021-08-07 01:13:05 +0300

    Main 12
    -Implemented game states

--- EndlessRunner/Game.cpp
+++ EndlessRunner/Game.cpp
@@ -1,11 +1,169 @@
+#include <sstream>
 #include "AudioManager.h"
 #include "TextureManager.h"
 #include "FontManager.h"
 #include "RenderManager.h"
 #include "CollisionManager.h"
+#include "Easing.h"
 #include "Game.h"
 
-Game::Game() : screen(this), quit(false)
+#define D_TIME 0.16f
+#define EASING_DURATION 1.45f
+
+#pragma region FSM
+GameState::~GameState() {}
+
+void GameState::Render(Game& game_in)
+{
+	game_in.m_background->Render();
+	game_in.m_player->Render();
+	game_in.m_opossum->Render();
+	game_in.m_eagle->Render();
+	game_in.m_text->Render();
+}
+
+#pragma region MenuState
+MenuState::MenuState() { m_easingTime = 0.f; }
+
+MenuState::~MenuState() {}
+
+void MenuState::Enter(Game& game_in)
+{
+	game_in.m_text->text = "Press SPACE to begin!";
+	game_in.m_text->transform.SetPosition(160.0f, 220.0f);
+	game_in.m_text->SetColor(SDL_Color{ 0, 255, 255, 255 });
+
+	game_in.m_score = 0.0f;
+
+	game_in.m_background->Reset();
+	game_in.m_player->Reset();
+	game_in.m_opossum->Reset();
+	game_in.m_eagle->Reset();
+
+	AudioManager::Instance()->PlayMusic("bg_music");
+}
+
+void MenuState::Render(Game& game_in)
+{
+	game_in.m_background->Render();
+	game_in.m_text->Render();
+}
+
+GameState* MenuState::OnKeyUp(Game& game_in, KeyCode key)
+{
+	if (key == KeyCode::SPACE)
+		return new PlayState();
+	return nullptr;
+}
+
+GameState* MenuState::Update(Game& game_in, const float dt)
+{
+	game_in.m_background->Update(dt);
+	game_in.UpdateEasing(m_easingTime, 6.f, 1.f);
+	return nullptr;
+}
+#pragma endregion
+
+#pragma region PlayState
+PlayState::PlayState() { m_easingTime = 0.f; }
+
+PlayState::~PlayState() {}
+
+void PlayState::Enter(Game& game_in)
+{
+	game_in.m_text->text = "SCORE: ";
+	game_in.m_text->transform.SetPosition(10.0f, 10.0f);
+	game_in.m_text->SetColor(SDL_Color{ 0, 255, 255, 255 });
+}
+
+GameState* PlayState::OnKeyUp(Game& game_in, KeyCode key)
+{
+	game_in.m_player->OnKeyUp(key);
+
+	if (key == KeyCode::P)
+		return new PauseState();
+	return nullptr;
+}
+
+GameState* PlayState::Update(Game& game_in, const float dt)
+{
+	game_in.m_score += (D_TIME * 10.f);
+	game_in.m_text->text = "Score: " + std::to_string(static_cast<int>(game_in.m_score));
+	game_in.UpdateEasing(m_easingTime, 6.f, 0.5f);
+
+	game_in.m_background->Update(dt);
+	game_in.m_opossum->Update(dt);
+	game_in.m_eagle->Update(dt);
+	game_in.m_player->Update(dt);
+
+	if (CollisionManager::CheckCollision(game_in.m_player->collider, game_in.m_opossum->collider)
+		|| CollisionManager::CheckCollision(game_in.m_player->collider, game_in.m_eagle->collider))
+		return new DeathState();
+
+	return nullptr;
+}
+#pragma endregion
+
+#pragma region PauseState
+PauseState::PauseState() { m_easingTime = 0.0f; }
+
+PauseState::~PauseState() {}
+
+void PauseState::Enter(Game& game_in)
+{
+	game_in.m_text->text = "PAUSED";
+	game_in.m_text->transform.SetPosition(280.0f, 220.0f);
+	game_in.m_text->SetColor(SDL_Color{ 0, 255, 255, 255 });
+}
+
+GameState* PauseState::OnKeyUp(Game& game_in, KeyCode key)
+{
+	if (key == KeyCode::P)
+		return new PlayState();
+	return nullptr;
+}
+GameState* PauseState::Update(Game& game_in, const float dt)
+{
+	game_in.UpdateEasing(m_easingTime, 6.f, 1.f);
+	return nullptr;
+}
+#pragma endregion
+
+#pragma region DeathState
+DeathState::DeathState() { m_easingTime = 0.f; }
+
+DeathState::~DeathState() {}
+
+void DeathState::Enter(Game& game_in) 
+{
+	game_in.m_player->SetDeathState();
+
+	std::ostringstream oss;
+	oss << "     Game Over! \n     Score: " << static_cast<int>(game_in.m_score) << "\nPress SPACE to restart!";
+	game_in.m_text->text = oss.str();
+	game_in.m_text->transform.SetPosition(200.0f, 200.0f);
+	game_in.m_text->SetColor(SDL_Color{ 255, 55, 0, 255 });
+}
+
+GameState* DeathState::OnKeyUp(Game& game_in, KeyCode key)
+{
+	if (key == KeyCode::SPACE)
+		return new MenuState();
+	return nullptr;
+}
+
+GameState* DeathState::Update(Game& game_in, const float dt)
+{
+	game_in.UpdateEasing(m_easingTime, 6.f, 0.75f);
+	game_in.m_player->Update(dt);
+	return nullptr;
+}
+#pragma endregion
+
+#pragma endregion
+
+
+Game::Game() : m_screen(this), m_quit(false), m_score(0.0f)
 {
 }
 
@@ -13,48 +171,51 @@ Game::~Game()
 {
 	printf("Destroy Game\n");
 
-	delete background;
-	delete player;
-	delete opossum;
-	delete eagle;
+	delete m_state;
+	delete m_background;
+	delete m_player;
+	delete m_opossum;
+	delete m_eagle;
 
 	AudioManager::Instance()->Clear();
 	TextureManager::Instance()->Clear();
 	SDL_DestroyRenderer(RenderManager::Instance()->GetRenderer());
-	SDL_DestroyWindow(screen.window);
+	SDL_DestroyWindow(m_screen.window);
 	IMG_Quit();
 	SDL_Quit();
 }
 
-void Game::CheckCollisons()
+void Game::UpdateEasing(float& t, const float d, const float c)
 {
-	if (CollisionManager::CheckCollision(player->collider, opossum->collider) 
-		|| CollisionManager::CheckCollision(player->collider, eagle->collider))
-		player->Die();
-}
-
-void Game::UpdateScore()
-{
-	int currentScore = SDL_GetTicks() / 10;
-	score->text = "Score: " + std::to_string(currentScore);
+	t += D_TIME;
+	if (t > d)
+		t = d;
+	m_text->transform.SetScale(Easing::EaseOut(t, d, c), Easing::EaseOut(t, d, c));
 }
 
 void Game::OnKeyUp(KeyCode key)
 {
-	player->OnKeyUp(key);
+	GameState* state = m_state->OnKeyUp(*this, key);
+	if (state != NULL)
+	{
+		delete m_state;
+		m_state = state;
+
+		m_state->Enter(*this);
+	}
 }
 
 void Game::OnKeyDown(KeyCode key)
 {
 	if (key == KeyCode::ESCAPE)
-		quit = true;
+		m_quit = true;
 }
 
 
 void Game::Init()
 {
-	screen.Init();
-	RenderManager::Instance()->Init(screen);
+	m_screen.Init();
+	RenderManager::Instance()->Init(m_screen);
 
 	AudioManager::Instance()->LoadMusic("bg_music", "Assets/Audio/bgmusic.wav");
 	AudioManager::Instance()->LoadSound("jump_sound", "Assets/Audio/jump.wav");
@@ -74,42 +235,34 @@ void Game::Init()
 
 	FontManager::Instance()->Load("font_main", "Assets/Fonts/quite_magical.ttf", 64);
 
-	background = new Parallax(35.0f);
-	player = new Player();
-	opossum = new Opossum();
-	eagle = new Eagle();
+	m_background = new Parallax(35.0f);
+	m_player = new Player();
+	m_opossum = new Opossum();
+	m_eagle = new Eagle();
 
-	score = new Text("font_main", SDL_Color{255, 255, 255, 255});
-	score->transform.SetPosition(10, 10);
+	m_text = new Text("font_main", SDL_Color{255, 255, 255, 255});
 
-	AudioManager::Instance()->PlayMusic("bg_music");
+	m_state = new MenuState();
+	m_state->Enter(*this);
 }
 
 void Game::Update(const float dt)
 {
-	if (player->isAlive)
+	GameState* state = m_state->Update(*this, D_TIME);
+	if (state != NULL)
 	{
-		CheckCollisons();
-		UpdateScore();
+		delete m_state;
+		m_state = state;
 
-		background->Update(dt);
-		opossum->Update(dt);
-		eagle->Update(dt);
+		m_state->Enter(*this);
 	}
-
-	player->Update(dt);
 }
 
 void Game::Render()
 {
 	RenderManager::Instance()->Clear(255, 255, 255, 255);
 
-	background->Render();
-	player->Render();
-	opossum->Render();
-	eagle->Render();
-
-	score->Render();
+	m_state->Render(*this);
 
 	RenderManager::Instance()->Present();
 }
@@ -119,23 +272,19 @@ void Game::Run()
 {
 	Init();
 
-	SDL_SetRenderDrawColor(RenderManager::Instance()->GetRenderer(), 255, 255, 255, 255);
-	SDL_RenderClear(RenderManager::Instance()->GetRenderer());
-
 	SDL_Event event;
-	while (!quit)
+	while (!m_quit)
 	{
 		while (SDL_PollEvent(&event)) {
 			switch (event.type)
 			{
-			case SDL_QUIT: quit = true; break;
+			case SDL_QUIT: m_quit = true; break;
 			case SDL_KEYDOWN: OnKeyDown(TranslateKeyCode(event.key.keysym.sym)); break;
 			case SDL_KEYUP: OnKeyUp(TranslateKeyCode(event.key.keysym.sym)); break;
 			}
 		}
 
-		Update(deltaTime);
+		Update(D_TIME);
 		Render();
 	}
 }
-
--- EndlessRunner/Game.h
+++ EndlessRunner/Game.h
@@ -8,17 +8,92 @@
 #include "Enemy.h"
 #include "KeyCode.h"
 
-#define deltaTime 0.16f
+#pragma region FSM
+class GameState
+{
+public:
+	virtual ~GameState() = 0;
+
+	virtual void Enter(Game& game_in) = 0;
+	virtual void Render(Game& game_in);
+	virtual GameState* OnKeyUp(Game& game_in, KeyCode key) = 0;
+	virtual GameState* Update(Game& game_in, const float dt) = 0;
+protected:
+	float m_easingTime;
+};
+
+class MenuState : public GameState
+{
+public:
+	MenuState();
+	~MenuState();
+
+	void Enter(Game& game_in) override;
+	void Render(Game& game_in) override;
+	GameState* OnKeyUp(Game& game_in, KeyCode key) override;
+	GameState* Update(Game& game_in, const float dt) override;
+};
+
+class PlayState : public GameState
+{
+public:
+	PlayState();
+	~PlayState();
+
+	void Enter(Game& game_in) override;
+	GameState* OnKeyUp(Game& game_in, KeyCode key) override;
+	GameState* Update(Game& game_in, const float dt) override;
+};
+
+class PauseState : public GameState
+{
+public:
+	PauseState();
+	~PauseState();
+
+	void Enter(Game& game_in) override;
+	GameState* OnKeyUp(Game& game_in, KeyCode key) override;
+	GameState* Update(Game& game_in, const float dt) override;
+};
+
+class DeathState : public GameState
+{
+public:
+	DeathState();
+	~DeathState();
+
+	void Enter(Game& game_in) override;
+	GameState* OnKeyUp(Game& game_in, KeyCode key) override;
+	GameState* Update(Game& game_in, const float dt) override;
+};
+#pragma endregion
 
 class Game
 {
-	bool quit;
-	Screen screen;
-	Opossum* opossum;
-	Eagle* eagle;
-	Player* player;
-	Parallax* background;
-	Text* score; 
+public:
+	Game();
+	~Game();
+
+	void Run();
+
+private:
+	//Friend declaration
+	friend class GameState;
+	friend class MenuState;
+	friend class PlayState;
+	friend class PauseState;
+	friend class DeathState;
+
+	bool m_quit;
+	float m_score;
+	Screen m_screen;
+	GameState* m_state;
+
+	Opossum* m_opossum;
+	Eagle* m_eagle;
+	Player* m_player;
+	Parallax* m_background;
+	Text* m_text; 
 
 	//Game Loop
 	void OnKeyUp(KeyCode key);
@@ -28,13 +103,5 @@ class Game
 	void Update(const float dt);
 
 	//Other
-	void CheckCollisons();
-	void UpdateScore();
-
-public:
-	Game();
-	~Game();
-
-	void Run();
+	void UpdateEasing(float& t, const float d, const float c);
 };
-
